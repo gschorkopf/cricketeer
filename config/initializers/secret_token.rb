82d68336452ce50e8e883e351fd3dfb53264424a
Geoff Schorkopf <gschorkopf@gmail.com>
2013-11-22 18:24:30 -0500

    First commit on an empty Rails 4.0 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cricketeer::Application.config.secret_key_base = '99556ac8d2afc8cabf88f425b1e6895f4d85ee2b54861f6d98424b27b8fb54fa8360010a6a4e26626f61c332fe6fa040307722e53a81d15e12965f0d9d85e36b'
